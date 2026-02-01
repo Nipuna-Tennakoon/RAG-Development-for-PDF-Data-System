f1717d587dac21fd927f45efa171af70e7fb6fe7
Nipuna-Tennakoon <tennakoon1997@gmail.com>
2026-02-01 15:02:20 +0530

    indexing pipeline is implemented

--- src/extract.py
+++ src/extract.py
@@ -1,21 +1,33 @@
 from langchain_community.document_loaders import PyMuPDFLoader
-
+from pathlib import Path
 
 data_dir = "../data"
 output_dir = "../results"
 
 def main():
-    extracted_texts = open(f"{output_dir}/extracted_texts.txt", "wb")
-    loader = PyMuPDFLoader(f"{data_dir}/RAG development.pdf", mode = "page")
-    docs = loader.load()
-    page_count = len(docs)
-
-    for i in range(10):
-        texts = docs[i].page_content.encode("utf-8")
-        extracted_texts.write(texts)
-        extracted_texts.write(bytes((12,)))
+    
+    
+    path = Path(data_dir)
+    if path.is_dir():
+        files = [item.name for item in path.iterdir() if item.is_file()]
+        print(files)
+    else:
+        print("The directory does not exists.")
         
-    extracted_texts.close()
+       
+    for file in files:
+        loader = PyMuPDFLoader(f"{data_dir}/{file}", mode="page")
+        doc = loader.load()
+        print(f"Page count of{file}:{len(doc)}")
+        
+        with open(f"{output_dir}/{file[:-2]}.txt", "w", encoding="utf8", ) as file_1:
+            with open(f"{output_dir}/{file[:-2]}_metadata.txt", "w", encoding="utf8") as file_2:
+                for i in range(len(doc)):
+                    file_1.write(doc[i].page_content[:1000]+"\n")
+                    
+                    file_2.write(str(doc[i].metadata)+"\n")
+                    
+            
 
 if __name__ == "__main__":
     main()
